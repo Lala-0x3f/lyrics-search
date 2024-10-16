040b10b37adcc9fad23c113ae13ce4077ada63dc
lala0x3f <93862679+Lala-0x3f@users.noreply.github.com>
2024-10-16 10:55:11 +0800

    Update songlist.tsx

--- components/songlist.tsx
+++ components/songlist.tsx
@@ -24,34 +24,35 @@ const SongsList = ({
   song: LrcsResult[];
   onChoice: (id: string) => void;
 }) => {
-  if (song)
-    return (
-      <ul
-        className='grid justify-start w-full gap-2 px-2 overflow-y-scroll sm:grid-cols-2 md:grid-cols-3 '
-        style={{ scrollbarWidth: 'none' }}
-      >
-        {song.map((song, index) => (
-          <li
-            key={index}
-            className='w-full h-[3rem] group flex gap-2 rounded-2xl hover:bg-secondary-50/50 transition-all duration-100 cursor-pointer overflow-clip'
-            onClick={() => {
-              onChoice(song.id);
-            }}
-          >
-            <div className='size-[3rem] absolute'>
-              <ImageItem album={song.album} />
-            </div>
-            <div className='p-1 text-sm overflow-clip text-nowrap pl-[3.5rem]'>
-              <h5 className='font-semibold'>
-                {song.title} -{' '}
-                <span className='italic text-secondary/50'>{song.album}</span>
-              </h5>
-              <p>{song.artist}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-    );
+  return (
+    <ul
+      className='grid justify-start w-full gap-2 px-2 overflow-y-scroll sm:grid-cols-2 md:grid-cols-3 '
+      style={{ scrollbarWidth: 'none' }}
+    >
+      {song.map((song, index) => (
+        <li
+          key={index}
+          className='w-full h-[3rem] group flex gap-2 rounded-2xl hover:bg-secondary-50/50 transition-all duration-100 cursor-pointer overflow-clip'
+          onClick={() => {
+            onChoice(song.id);
+          }}
+        >
+          <div className='size-[3rem] absolute'>
+            <ImageItem album={song.album} />
+          </div>
+          <div className='p-1 text-sm overflow-clip text-nowrap pl-[3.5rem]'>
+            <h5 className='font-semibold'>
+              {song.title} -{' '}
+              <span className='italic text-secondary/50'>{song.album}</span>
+            </h5>
+            <p>{song.artist}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+
+  return null;
 };
 
 export default SongsList;
